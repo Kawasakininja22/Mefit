e1a6c0c1931e3dc147a272649a4b1d8ff64bd061
poorni_4622 <Kawasakininja22@users.noreply.github.com>
2024-07-15 15:40:28 +0530

    Updates

---
+++ precache-manifest.31ff980a7d1886a907b9cc410afd5146.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384b104919d79017a7fd33db51612817",
+    "url": "/Mefit/index.html"
+  },
+  {
+    "revision": "043bd2f8e460fe96a43a",
+    "url": "/Mefit/static/css/2.8ac840ac.chunk.css"
+  },
+  {
+    "revision": "48e2cbea4e66d88058e1",
+    "url": "/Mefit/static/css/main.9870cec0.chunk.css"
+  },
+  {
+    "revision": "043bd2f8e460fe96a43a",
+    "url": "/Mefit/static/js/2.9506257d.chunk.js"
+  },
+  {
+    "revision": "a380a4c58228e1a19ed1e19105eb0602",
+    "url": "/Mefit/static/js/2.9506257d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f61b33c4176ba046dbb",
+    "url": "/Mefit/static/js/3.5b4a847c.chunk.js"
+  },
+  {
+    "revision": "48e2cbea4e66d88058e1",
+    "url": "/Mefit/static/js/main.7ebafb68.chunk.js"
+  },
+  {
+    "revision": "5fc152af5b3f40c81137",
+    "url": "/Mefit/static/js/runtime-main.68b2230c.js"
+  },
+  {
+    "revision": "ec009df898b4b34b7ee3b48704445eef",
+    "url": "/Mefit/static/media/Rev1.ec009df8.png"
+  },
+  {
+    "revision": "9a89beec7dd7fa072daa0c8fa0c28d1b",
+    "url": "/Mefit/static/media/Rev2.9a89beec.png"
+  },
+  {
+    "revision": "2342132eef97f3b46d577e9ed773f794",
+    "url": "/Mefit/static/media/back.2342132e.png"
+  },
+  {
+    "revision": "6fd22488181ae0782b8765d4aad8d15e",
+    "url": "/Mefit/static/media/back1.6fd22488.png"
+  },
+  {
+    "revision": "dbd65f517d423d3806c8946dc96dfc99",
+    "url": "/Mefit/static/media/eclp.dbd65f51.png"
+  },
+  {
+    "revision": "2acd309890e6e91f3933627df5f71cb4",
+    "url": "/Mefit/static/media/fruitbowl.2acd3098.png"
+  },
+  {
+    "revision": "35f074e7d73c73d8e608bc3d72c8d307",
+    "url": "/Mefit/static/media/grapes.35f074e7.png"
+  },
+  {
+    "revision": "ee3446732bd4be5a9cc1145126ced152",
+    "url": "/Mefit/static/media/meatbowl.ee344673.png"
+  },
+  {
+    "revision": "8f4c82551c2bb685e8c03b9e454f52a9",
+    "url": "/Mefit/static/media/veg.8f4c8255.png"
+  },
+  {
+    "revision": "aaecef7c816c39b08dad234621b3a536",
+    "url": "/Mefit/static/media/vegbowl.aaecef7c.png"
+  }
+]);
